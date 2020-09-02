02bab19a0ea991b7b15f751caa13a6d6ac6ab34e
KeanuReeves98 <153587104@qq.com>
2020-09-02 14:18:06 +0800

    冒泡排序完整版

--- test1/src/hx/test1.java
+++ test1/src/hx/test1.java
@@ -1,11 +1,20 @@
 package hx;
 
+import java.util.Arrays;
+
 public class test1 {
     public static void main(String[] args) {
         int[] array={3,0,7,4,8,74,9};
-        for (int i = 0; i < array.length ; i++)
+        for (int i = 0; i < array.length-1 ; i++)
         {
+            for (int j = 0; j < array.length-1-i ; j++)
+            {
+                int a=array[j];
+                array[j]=array[j+1];
+                array[j+1]=a;
 
+            }
         }
+        System.out.println(Arrays.toString(array));
     }
 }
