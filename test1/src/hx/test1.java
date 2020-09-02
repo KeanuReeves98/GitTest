d25b930bf5aaf9ea2a1343078e555b440f0d89fd
KeanuReeves98 <153587104@qq.com>
2020-09-02 10:58:48 +0800

    冒泡排序外循环

--- test1/src/hx/test1.java
+++ test1/src/hx/test1.java
@@ -2,6 +2,10 @@ package hx;
 
 public class test1 {
     public static void main(String[] args) {
-        int[] array={1,9,8};
+        int[] array={3,0,7,4,8,74,9};
+        for (int i = 0; i < array.length ; i++)
+        {
+
+        }
     }
 }
